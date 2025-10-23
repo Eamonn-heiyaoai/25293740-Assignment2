18b1fa1bd6a8a922c1b4486b4536b573e59a8923
Heiyaoai <14310902+heiyaoai@user.noreply.gitee.com>
2025-10-23 18:07:53 +1300

    final result

---
+++ course-catalogue/app.py
@@ -0,0 +1,30 @@
+﻿from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+courses = [
+    {"id": 1, "code": "COMP601", "name": "Software Engineering"},
+    {"id": 2, "code": "COMP604", "name": "Database Systems"}
+]
+
+@app.route('/courses', methods=['GET'])
+def get_courses():
+    return jsonify(courses), 200
+
+@app.route('/courses', methods=['POST'])
+def add_course():
+    new_course = request.get_json()
+    if not new_course.get("id") or not new_course.get("name"):
+        return jsonify({"error": "Missing id or name field"}), 400
+    courses.append(new_course)
+    return jsonify({"message": "Course added successfully", "data": new_course}), 201
+
+@app.route('/courses/<int:course_id>', methods=['GET'])
+def get_course_by_id(course_id):
+    for course in courses:
+        if course["id"] == course_id:
+            return jsonify(course), 200
+    return jsonify({"error": "Course not found"}), 404
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ feedback/app.py
@@ -0,0 +1,40 @@
+﻿from flask import Flask, jsonify, request
+import requests
+import os
+
+app = Flask(__name__)
+
+STUDENT_SERVICE = os.getenv("STUDENT_SERVICE", "http://student-profile:5000")
+COURSE_SERVICE = os.getenv("COURSE_SERVICE", "http://course-catalogue:5000")
+
+feedback_list = []
+
+@app.route('/feedback', methods=['POST'])
+def submit_feedback():
+
+    data = request.get_json()
+
+    if not data or not data.get("student_id") or not data.get("course_id") or not data.get("comment"):
+        return jsonify({"error": "Missing student_id, course_id or comment"}), 400
+
+    feedback_list.append(data)
+
+    try:
+        student_response = requests.get(f"{STUDENT_SERVICE}/students").json()
+        course_response = requests.get(f"{COURSE_SERVICE}/courses").json()
+    except Exception as e:
+        return jsonify({"message": "Feedback stored but failed to fetch other data", "error": str(e)}), 500
+
+    return jsonify({
+        "message": "Feedback received!",
+        "feedback": data,
+        "student_count": len(student_response),
+        "course_count": len(course_response)
+    }), 201
+
+@app.route('/feedback', methods=['GET'])
+def get_all_feedback():
+    return jsonify(feedback_list), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
